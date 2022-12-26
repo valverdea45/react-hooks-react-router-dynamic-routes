5a794339e872bb858fedb9dd20f40d2cd2a8217e
Adrian <adrianvalverde45@gmail.com>
2022-12-26 15:08:07 -0500

    commit tree 8c1b94e517db693eb947ecfe7bf42a2ce3f4db04

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow"
 
 function MoviesPage({ movies }) {
+
+  const match = UseRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route>
+        <h3> Choose a movie from the list above</h3>
+      </Route>
+
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
